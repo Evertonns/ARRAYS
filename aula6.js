c3052e3be8d80468dd5bc04c0f79e78f6f827b66
Everton Silva <evertonns.dev@gmail.com>
2022-07-28 18:52:59 -0300

    aula6

---
+++ aula6.js
@@ -0,0 +1,9 @@
+//Desafio - juntando salas
+
+const salaDePython = ["Melissa", "Helena", "Rodrigo"];
+
+const salaDeJavaScript = ["Ju", "Leo", "Raquel"];
+
+const salaUnificadas = salaDePython.concat(salaDeJavaScript);
+
+console.log(salaUnificadas);
