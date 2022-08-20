c45e1a5ec03c0654325c1815033342e6eac4a61f
Everton Silva <evertonns.dev@gmail.com>
2022-08-20 19:51:06 -0300

    aula10

---
+++ aula10.js
@@ -0,0 +1,14 @@
+//Desafio - Média
+// Caucular a média entre as seguintes notas usando o For: 10, 6,5, 8 e 7.5
+
+const notas = [10, 6.5, 8, 7.5];
+
+let somaDasNotas = 0;
+
+for (let i = 0; i < notas.length; i++) {
+  somaDasNotas += notas[i];
+}
+
+let media = somaDasNotas / notas.length;
+
+console.log(media);
