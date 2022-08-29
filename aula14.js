a40a8c12c2173a536ce470221ca0dec21dd0c48b
Everton Silva <evertonns.dev@gmail.com>
2022-08-29 20:17:28 -0300

    aula14

---
+++ aula14.js
@@ -0,0 +1,9 @@
+//Desafio - Padronizando os nomes
+// Precisamos padronizar a lista de alunos para conter apenas letras maiúsculas;
+// ['ana Julia', 'Caio vinicius', 'Bia silva']
+
+let nomes = ["ana Julia", "Caio vinicius", "Bia silva"];
+
+const nomesAtualizados = nomes.map((nome) => nome.toUpperCase());
+
+console.log(nomesAtualizados);
