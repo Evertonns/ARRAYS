ca1fa4feeff561b2dc36834fbf0e8df7aeaf707f
Everton Silva <evertonns.dev@gmail.com>
2022-09-06 15:43:52 -0300

    aula16

---
+++ aula16.js
@@ -0,0 +1,18 @@
+//Desafio - medias das turmas
+//Com a média de todos os alunos de 3 salas, calcule a média geral de cada sala:
+//- const salaJS = [7, 8, 8, 7, 10, 6.5, 4, 10, 7]
+//- const salaJava = [6, 5, 8, 9, 5, 6]
+//- const salaPython = [7, 3.5, 8, 9.5]
+
+const salaJS = [7, 8, 8, 7, 10, 6.5, 4, 10, 7];
+const salaJava = [6, 5, 8, 9, 5, 6];
+const salaPython = [7, 3.5, 8, 9.5];
+
+function mediaSala(notasDaSala) {
+  const somaDasNotas = notasDaSala.reduce((acum, atual) => atual + acum, 0);
+  return somaDasNotas / notasDaSala.length;
+}
+
+console.log(`Média da sala de javaScript ${mediaSala(salaJS)}`);
+console.log(`Média da sala de java ${mediaSala(salaJava)}`);
+console.log(`Média da sala de Python ${mediaSala(salaPython)}`);
